41c839aa050842c24359aabadba5453be280f210
Dmitrijs Juskovs <bober12@inbox.lv>
2021-11-27 01:59:02 +0200

    Simple folder-based playlist implemented

---
+++ StreamControlLite/Extensions/MediaLibrary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FtpudStreamFramewok.Core;
+using FtpudStreamFramewok.Source;
+
+namespace StreamControlLite.Extensions
+{
+    public class MediaLibrary
+    {
+        private List<FileSourceEntity> _fileList;
+
+        private bool IsValidVideoExtension(string filename)
+        {
+            return filename.EndsWith(".ts")
+                   || filename.EndsWith(".mp4")
+                   || filename.EndsWith(".mkv")
+                   || filename.EndsWith(".m2ts");
+        }
+
+        public void Init()
+        {
+            int listNum = 0;
+            InputProcessor.Instance().OnFinished += (sender, eventArgs) =>
+            {
+                listNum++;
+                if (listNum == _fileList.Count) listNum = 0;
+                InputProcessor.Instance().Play(_fileList[listNum]);
+            };
+        }
+
+        public void Start()
+        {
+            InputProcessor.Instance().Play(_fileList[0]);
+        }
+
+        public void Load(String path)
+        {
+            _fileList = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                .Where(IsValidVideoExtension)
+                .Select(fileName => new FileSourceEntity(fileName))
+                .ToList();
+        }
+
+
+        private static MediaLibrary _instance;
+
+        public static MediaLibrary instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new MediaLibrary();
+            }
+
+            return _instance;
+        }
+    }
+}
--- StreamControlLite/Program.cs
+++ StreamControlLite/Program.cs
@@ -7,6 +7,7 @@ using FtpudStreamFramewok.Settings.Filters;
 using FtpudStreamFramewok.Source;
 using FtpudStreamFramewok.Target;
 using FtpudStreamFramewok.Util;
+using StreamControlLite.Extensions;
 
 
 namespace StreamControlLite
@@ -31,24 +32,15 @@ namespace StreamControlLite
             Publisher publisher = new Publisher();
             publisher.RunPublisher(new StreamRtmpTarget("rtmp://192.168.0.129/test"));
 
-            List<FileSourceEntity> playList = new List<FileSourceEntity>()
-            {
-                new FileSourceEntity("Z:\\_idoling\\dl\\Task have Fun - 3WD (MV).mp4"),
-                new FileSourceEntity("Y:\\wasutahq\\wasuta - Meranyaizar [HQ].ts"),
-                new FileSourceEntity("Q:\\_dav\\RequestOnly\\Geneki Idol ga Erabu! Shimakura Rika  Ririka - Scream.mp4"),
-                new FileSourceEntity("Z:\\_idoling\\dl\\Task have Fun - 星フルWISH (MV).mp4"),
-                new FileSourceEntity("Z:\\_idoling\\dl\\Task have Fun - Kimi nanda kara (MV).mp4"),
-            };
-
-            int listNum = 0;
-            InputProcessor.Instance().OnFinished += (sender, eventArgs) =>
-            {
-                listNum++;
-                if (listNum == playList.Count) listNum = 0;
-                InputProcessor.Instance().Play(playList[listNum]);
-            };
-            InputProcessor.Instance().Play(playList[listNum]);
             
+            MediaLibrary.instance().Init();
+            MediaLibrary.instance().Load("Y:\\wasutahq\\");
+            
+
+            
+            // Start
+            MediaLibrary.instance().Start();
+
 
             while (true)
             {
