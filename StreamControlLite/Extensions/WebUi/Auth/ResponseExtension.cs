cdb3e0af78eaf7f79cdafa62ffa0de0c8b7ab222
Dmitrijs Juskovs <bober12@inbox.lv>
2022-05-29 18:48:07 +0300

    Service implementation is moved to another git repository

--- StreamControlLite/Extensions/WebUi/Auth/ResponseExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace StreamControlLite.Extensions.WebUi.Auth
-{
-    public abstract class ResponseExtension
-    {
-        public abstract WebServerResponse intercept(HttpListenerRequest request);
-    }
-}
